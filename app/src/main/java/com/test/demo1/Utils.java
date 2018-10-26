50578645bb13beab575217d5c93744eff3aed430
palana <309516741@qq.com>
2018-10-26 16:53:54 +0800

    create Dev class

--- app/src/main/java/com/test/demo1/Utils.java
+++ app/src/main/java/com/test/demo1/Utils.java
@@ -1,10 +1,11 @@
 package com.test.demo1;
 
 public class Utils {
-    public void function(){
-        int i=0;
-        long l=0;
-        String str="";
-        Object obj=null;
+    public void function() {
+        int i = 0;
+        long l = 0;
+        String str = "";
+        Object obj = null;
+        int i2 = 0;
     }
 }
